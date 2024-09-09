d11a8afbddf99ffb711c89b6c10f5f126424c14f
lnvpatel <lnvpatel@gmail.com>
2024-09-09 20:01:42 +0530

    start server file update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,11 @@ cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_ID/deployment-archive
 
 # Start the server in the background
 # Start the server and bind it to all network interfaces
-serve -p 8080 --listen 0.0.0.0 . &
+#!/bin/bash
+# Start the server using serve and run it in the background
+
+serve -s . -p 8080 >> /tmp/start_server.log 2>&1 &
+
 
 # Optional: Log output
 echo "Application started successfully!" >> /tmp/start_server.log
